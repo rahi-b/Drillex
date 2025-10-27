fb032ba98b4c766a04a129164a77f4b204812a1c
marshook ali <marshookali98@gmail.com>
2025-10-27 22:54:52 +0530

    emptycart issue

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { useSelector,useDispatch } from "react-redux";
-import EmptyCart from "../assets/images/empty.png";
+import EmptyCart from "../assets/images/EmptyCart.png";
 import { FaTrashAlt } from "react-icons/fa";
 import Modal from "../components/Modal";
 import ChangeAddress from "../components/ChangeAddress";
