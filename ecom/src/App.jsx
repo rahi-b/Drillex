9732854a21011806a33ed02a8dc6369cc41edb67
marshook ali <marshookali98@gmail.com>
2025-10-27 15:57:30 +0530

    browser router issue solved

--- ecom/src/App.jsx
+++ ecom/src/App.jsx
@@ -1,5 +1,7 @@
 import Navbar from "./components/Navbar"
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+// import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom"
+
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
 import Shop from "./pages/Shop"
--- ecom/vite.config.js
+++ ecom/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss()
   ],
+  base: '/drillax/', // 👈 add this line
   
 })
 
